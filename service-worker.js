8fc08164ae0af31e74387235d971d25ded8440c9
dlyncha <dave.j.lynch92@gmail.com>
2025-12-04 17:09:41 +0930

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,10 @@
 const CACHE_NAME = "job-tracker-cache-v1";
 
 const FILES_TO_CACHE = [
-  "/job-tracker/",          // Your main URL (folder name)
-  "/job-tracker/index.html",
-  "/job-tracker/service-worker.js",
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./service-worker.js"
 ];
 
 self.addEventListener("install", event => {
@@ -17,9 +18,7 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
       )
     )
   );
@@ -28,7 +27,7 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => 
+    caches.match(event.request).then(response =>
       response || fetch(event.request)
     )
   );
