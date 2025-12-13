674e802ae9ef3e01a71b605460596785a426596c
dlyncha <dave.j.lynch92@gmail.com>
2025-12-13 15:58:27 +0930

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // service-worker.js
 
-const CACHE_NAME = 'job-tracker-v8'; // increment this manually if needed
+const CACHE_NAME = 'job-tracker-v9'; // increment this manually if needed
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
